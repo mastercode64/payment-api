edc198aac13fd1a2fa223a1792519c10d0168fd4
Marcos Fujihashi <marcosfujihashi@gmail.com>
2019-04-20 00:39:46 -0300

    Added validation to the payment class

--- src/main/java/com/mastercode/paymentapi/domain/entity/Payment.java
+++ src/main/java/com/mastercode/paymentapi/domain/entity/Payment.java
@@ -1,38 +1,61 @@
 package com.mastercode.paymentapi.domain.entity;
 
+import java.math.BigDecimal;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
 
 import com.mastercode.paymentapi.domain.PaymentType;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
-@Entity
 public class Payment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String clientId;
+	@NotBlank
+	private String clientID;
 
+	@NotBlank
 	private String buyerName;
+
+	@NotBlank
+	@Email
 	private String buyerEmail;
+
+	@NotBlank
+	@CPF
 	private String buyerCPF;
 
-	@Enumerated(EnumType.STRING)
-	private PaymentType paymentType;
+	@NotNull
+	@Min(value = 50)
+	private BigDecimal value;
 
-	@ManyToOne
-	private CreditCard creditCard;
-	
+	@NotNull
+	private PaymentType type;
+
 	private String boletoNumber;
+
+	@OneToOne(orphanRemoval = true, cascade = CascadeType.ALL)
+	private CreditCard creditCard;
 
 }
