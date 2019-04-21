a23972abbb001e9435b77be751ad0f90b5d2868e
Marcos Fujihashi <marcosfujihashi@gmail.com>
2019-04-21 00:35:11 -0300

    Removed unused service class

--- src/main/java/com/mastercode/paymentapi/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.mastercode.paymentapi.service;
-
-import com.mastercode.paymentapi.domain.entity.Payment;
-
-public interface PaymentService {
-	Payment createPayment(Payment payment);
-	Payment findPayment(Long id);
-	void checkout(Payment payment);
-}
